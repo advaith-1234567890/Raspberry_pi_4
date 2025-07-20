d8ab64c89bfab1b82dfae1dc49b17b5cd674513a
advaith-1234567890 <advaith.v20@gmail.com>
2025-07-20 11:11:50 +0530

    Add new file

---
+++ blink.py
@@ -0,0 +1,14 @@
+from gpiozero import LED
+from time import sleep
+
+led = LED(18)
+
+try:
+    while True:
+        led.on()
+        sleep(1)
+        led.off()
+        sleep(1)
+
+except KeyboardInterrupt:
+    led.close()
